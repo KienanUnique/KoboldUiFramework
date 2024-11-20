f3fa642585e6a1d918d863fd0ff9fb0aca008a71
Kienan <69349336+KienanUnique@users.noreply.github.com>
2024-11-20 17:43:40 +0500

    KOB-50 add unirx for sample

---
+++ Samples/Simple Sample/Plugins/UniRx/Scripts/Observable.Blocking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SampleUnirx
+{
+    public static partial class Observable
+    {
+        public static T Wait<T>(this IObservable<T> source)
+        {
+            return new SampleUnirx.Operators.Wait<T>(source, InfiniteTimeSpan).Run();
+        }
+
+        public static T Wait<T>(this IObservable<T> source, TimeSpan timeout)
+        {
+            return new SampleUnirx.Operators.Wait<T>(source, timeout).Run();
+        }
+    }
+}
