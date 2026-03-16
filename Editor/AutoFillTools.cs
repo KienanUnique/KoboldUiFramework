c399fa808fa442be2bd567ae48681f72321fcc11
KIenan <69349336+KienanUnique@users.noreply.github.com>
2026-03-17 03:14:13 +0500

    namespace issue fix

--- Editor/AutoFillTools.cs
+++ Editor/AutoFillTools.cs
@@ -2,7 +2,7 @@
 using UnityEditor;
 using UnityEngine;
 
-namespace Editor
+namespace KoboldUiEditor
 {
     /// <summary>
     /// Provides editor utilities for automatically wiring window and view references in prefabs.
